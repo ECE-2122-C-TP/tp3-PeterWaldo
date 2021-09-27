f6f565cae2ee79e56989c1da9dd6bc848312f993
Pierre Dauchy <91371579+PierreDauchy@users.noreply.github.com>
2021-09-27 18:42:04 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+
+#include "mesfonctions.h"
+#include <stdio.h>
+
+/*int main(void) {
+    printf ("%d", monAgePlusDix(30));
+    return 0;
+}*/
+
+/*int main(){
+    printf("%d", tarifBillet());
+    return 0;
+}
+*/
+
+/*int main(){
+    printf("%f", moyenne ());
+    return 0;
+}*/
+
+int main() {
+    nombreTotalEleve();
+    return 0;
+}
---
+++ mesfonctions.h
@@ -0,0 +1,23 @@
+//
+// Created by pmdau on 20/09/2021.
+//
+
+#ifndef TD3_MESFONCTIONS_H
+#define TD3_MESFONCTIONS_H
+
+int plusGrandEntier(void);
+int perimetre (int l, int L);
+int aire (int l, int L);
+int afficherAireEtPerimetreRectangle (void);
+int ex3 (void);
+int tarifBillet(void);
+int distributeur (void);
+float moyenne3n (void);
+int nombreTotalEleve (void);
+int ex8(void);
+int pyramide(void);
+float moyenne (void);
+
+
+#endif //TD3_MESFONCTIONS_H
+
